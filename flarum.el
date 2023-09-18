e91fbe2931e5f04453ebca03e42809dbef3c842d
DarkBuffalo <darkbuffalo@gnu.re>
2023-09-18 10:04:17 +0200

    add: ID in tablist

--- flarum.el
+++ flarum.el
@@ -1,26 +1,40 @@
-;;; flarum.el --- Flarum api                         -*- lexical-binding: t; -*-
+;;; flarum.el --- Flarum unofficial api -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022  Matthias David
 
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.1")(request))
 ;; Author: Matthias David <darkbuffalo@gnu.re>
-;; Keywords: com
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This file is NOT part of GNU Emacs.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Copyright (c) 2022-2023, Matthias David
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are
+;; met:
+;;
+;;   * Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+;;   * Redistributions in binary form must reproduce the above copyright
+;;     notice, this list of conditions and the following disclaimer in the
+;;     documentation and/or other materials provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+;; IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+;; TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+;; PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+;; OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; 
+;;
 
 ;;; Code:
 
@@ -28,9 +42,11 @@
 (require 'cl-lib)
 
 
+;;; variables
 (defvar flarum-discs '()
   "List of discussions displayed in the *flarum* buffer.")
 
+;;; Faces
 (defface flarum-title-face
   '((t :inherit font-lock-type-face))
   "Face used for the disc title.")
@@ -56,21 +72,18 @@
   (read-only-mode -1)
   (erase-buffer)
   (read-only-mode 1)
-  (setq tabulated-list-format `[("Titre" 50 t)])
+  (setq tabulated-list-format `[("ID" 3 t)
+				("Titre" ,(- (window-width) 23) t)])
   (setq tabulated-list-entries (mapcar #'flarum--insert-entry
 				       flarum-discs))
   (tabulated-list-init-header)
   (tabulated-list-print))
 
-
-
 (defun flarum--insert-entry (data)
   "Insert DATA into the current buffer."
   (list (flarum-disc-shareUrl data)
-	(vector (flarum--format-title (flarum-disc-title data))
-		)))
-
-
+	(vector (flarum-disc-id data)
+		(flarum--format-title (flarum-disc-title data)))))
 
 (defun flarum--discussions-api ()
   "Call the Flarum discussions api."
@@ -94,7 +107,7 @@
 (defun flarum-search ()
   (interactive)
   (let ((data (flarum--discussions-api)))
-    (setq flarum-discs data)
+     (setq flarum-discs data)
     (flarum-draw-buffer)))
 
 ;;;###autoload
